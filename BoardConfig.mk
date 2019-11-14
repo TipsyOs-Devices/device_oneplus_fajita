b074ba9de24f67b730d4e99cbd9acfb72aec7ed0
Christian Oder <myself5@carbonrom.org>
2019-11-14 23:22:16 +0100

    fajita: Adjust defconfig for split configs
    Change-Id: I607e1bd5ccfd1c826649d7bfcd02aed8ae0cf091

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 DEVICE_FRAMEWORK_MANIFEST_FILE += $(DEVICE_PATH)/framework_manifest.xml
 
 # Kernel
-TARGET_KERNEL_CONFIG := carbon_oneplus6_defconfig
+TARGET_KERNEL_CONFIG := carbon_oneplus6t_defconfig
 
 # inherit from the proprietary version
 -include vendor/oneplus/fajita/BoardConfigVendor.mk
